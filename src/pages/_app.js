bf89434ab05adbdfa491b63e4e8727c1deec7ba6
Taehee Yoon <th.yoon.dev@gmail.com>
2024-01-27 13:23:19 +0900

    feat : 부트스트랩 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import Footer from '../components/layout/Footer';
+import Header from '../components/layout/Header';
+
+//bootstrap template...........................
+import '../assets/css/bootstrap.min.css';
+import '../assets/css/animate.css';
+import '../assets/css/owl.carousel.min.css';
+import '../assets/css/all.css';
+// import '../assets/css/flaticon.css';
+import '../assets/css/themify-icons.css';
+import '../assets/css/magnific-popup.css';
+import '../assets/css/slick.css';
+import '../assets/css/style.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
 }
